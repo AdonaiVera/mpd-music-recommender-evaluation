0eaf1f05767bd31ff704726c60b33836edf69503
Aneesh <aneeshprasann@gmail.com>
2024-12-01 01:33:17 -0500

    chore: removed unnecessary import

--- models/rbm_torch.py
+++ models/rbm_torch.py
@@ -2,8 +2,6 @@ import os
 import torch
 import torch.nn as nn
 import torch.optim as optim
-import numpy as np
-import pandas as pd
 from torch.utils.data import DataLoader, TensorDataset
 
 import sys
